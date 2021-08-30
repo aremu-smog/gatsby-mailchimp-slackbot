c59e4f68731a2dfe7581e1fd81e4d4d5b2808bed
Aremu Smog <aremuoluwagbamila@gmail.com>
2021-08-30 11:50:00 +0100

    chore: log responses

--- src/api/mailchimp/no-of-subscribers.js
+++ src/api/mailchimp/no-of-subscribers.js
@@ -20,27 +20,26 @@ export const noOfMailchimpSubscribers = async channel => {
   await mailchimpAxiosInstance
     .get("lists")
     .then(response => {
-      const lists = response.lists
+      console.log(response)
+      //   const lists = response.lists
 
-      console.log(lists)
+      //   const list = lists[0]
 
-      const list = lists[0]
+      //   console.log(list)
 
-      console.log(list)
+      //   const { name, stats } = list
 
-      const { name, stats } = list
+      //   const { member_count, last_sub_date } = stats
 
-      const { member_count, last_sub_date } = stats
+      //   const date_updated = new Date(last_sub_date)
+      //   const date_updated_parsed = `${
+      //     months[date_updated.getMonth()]
+      //   } ${date_updated.getDate()}, ${date_updated.getFullYear()}`
 
-      const date_updated = new Date(last_sub_date)
-      const date_updated_parsed = `${
-        months[date_updated.getMonth()]
-      } ${date_updated.getDate()}, ${date_updated.getFullYear()}`
+      //   const message = `${name} has *${member_count}* subscribers as at ${date_updated_parsed}`
 
-      const message = `${name} has *${member_count}* subscribers as at ${date_updated_parsed}`
-
-      sendSlackMessage(channel, message)
-      //   return {
+      //   sendSlackMessage(channel, message)
+      //   //   return {
       //     status: 200,
       //     message: message,
       //   }
