459b114d7665ca080e38294280b9b86c89ee4c5d
Aremu Smog <aremuoluwagbamila@gmail.com>
2021-08-30 02:58:28 +0100

    fix: authorization header

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,5 +2,5 @@ const axios = require("axios")
 
 export const botAxiosInstance = axios.create({
   baseURL: `https://slack.com/api/`,
-  headers: { Authorization: `apiKey ${process.env.SLACK_AUTHORIZATION_CODE}` },
+  headers: { Authorization: `Bearer ${process.env.SLACK_AUTHORIZATION_CODE}` },
 })
